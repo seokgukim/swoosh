b790fc61f9261180ec6bc85e271083a800a81760
seokgukim <rokja97@naver.com>
2025-08-28 09:00:33 +0900

    feat: add main test script and initialize test module documentation

---
+++ app/test.py
@@ -0,0 +1,14 @@
+"""
+Main test script to run all tests.
+"""
+
+import unittest
+from .core.config import settings
+from .utils.logger import *
+
+# from .tests.test_module import TestModule
+
+
+if __name__ == "__main__":
+    add_file_handler(settings.LOG_PATH + "/test.log")
+    unittest.main()
