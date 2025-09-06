184d4d24cd40467f64b220430d5b0e48df1456e6
seokgukim <rokja97@naver.com>
2025-09-06 09:27:16 +0900

    fix: update router import and logging configuration in main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from .api.endpoints.router import router
+
+from .api.routes import api_router
 
 app = FastAPI()
 
-app.include_router(router)
+app.include_router(api_router)
 
 
 @app.get("/")
@@ -14,8 +15,8 @@ def read_root():
 if __name__ == "__main__":
     import uvicorn
     import os
-    from .core.config import settings
-    from .utils.logger import add_file_handler
+    from .core.config import *
+    from .core.logger import add_file_handler
 
-    add_file_handler(os.path.join(settings.LOG_PATH, "swoosh.log"))
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    add_file_handler(os.path.join(LOG_PATH(), "swoosh.log"))
+    uvicorn.run(app, host="0.0.0.0", port=PORT())
